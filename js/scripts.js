62d58a7dd8e580cc81413d68fd67a39c86f60413
Francesco Re <francesco.re1107@gmail.com>
2021-04-05 13:37:35 +0200

    Improved for small devices

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,14 @@ var tw = new Typewriter('#console', {
   cursor: "&block;"
 });
 
-tw.pasteString("Last login: " + new Date() + "<br><br>> ")
+tw.pasteString("Last login: " + moment().format('ddd MMM Do YYYY h:mm:ss') + "<br><br>> ")
     .pauseFor(500)
     .typeString('cat <strong>greetings.txt<strong>')
     .pauseFor(1000)
-    .callFunction(function(){document.getElementById("console").getElementsByTagName("span")[0].innerHTML += "<br><br>Hi👋<br>My name is <strong>Francesco&nbsp;Re</strong><br>I’m an italian developer and student<br><br>> ";})
-    //.pasteString("<br><br>Hi👋<br>My name is Francesco&nbsp;Re<br>I’m an italian developer and student<br><br>> ")
+    .callFunction(
+        function(){
+            document.getElementById("console")
+                    .getElementsByTagName("span")[0]
+                    .innerHTML += "<br><br>Hi👋<br>My name is <strong>Francesco&nbsp;Re</strong><br>I’m an italian developer and student<br><br>> ";
+        })
     .start();
