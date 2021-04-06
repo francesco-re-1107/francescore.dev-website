42a330d195a9991bc576010968a24e602d382014
Francesco Re <francesco.re1107@gmail.com>
2021-04-06 12:39:04 +0200

    Changed strings Fixed LinkedIn Added polimi link

--- js/scripts.js
+++ js/scripts.js
@@ -18,7 +18,7 @@ tw.pasteString("Last login: " + moment(lastLogin).format('ddd MMM Do YYYY H:mm:s
         function(){
             document.getElementById("console")
                     .getElementsByTagName("span")[0]
-                    .innerHTML += "<br><br>Hi👋<br>My name is <strong>Francesco&nbsp;Re</strong>.<br>I’m an italian developer and student.<br><br>$ ";
+                    .innerHTML += "<br><br>Hi👋<br>My name is <strong>Francesco&nbsp;Re</strong>.<br>I’m a developer and a student at <a href='https://www.polimi.it' target='_blank'>PoliMi</a>.<br><br>$ ";
 
             window.localStorage.lastLogin = new Date();
         })
