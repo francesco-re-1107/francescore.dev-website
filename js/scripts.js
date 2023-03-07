ebfe7bd05ac4c5388b036a3787e8fa2781825d29
Francesco Re <francesco.re1107@gmail.com>
2023-03-07 16:23:44 +0100

    Change description

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,7 @@ tw.pasteString("Last login: " + moment(lastLogin).format('ddd MMM Do YYYY H:mm:s
         function(){
             document.getElementById("console")
                     .getElementsByTagName("span")[0]
-                    .innerHTML += "<br><br>Hi👋<br>My name is <strong>Francesco&nbsp;Re</strong>.<br>I’m a developer and a student at <a href='https://www.polimi.it' target='_blank'>PoliMi</a>.<br><br>$ ";
+                    .innerHTML += "<br><br>Hi👋<br>My name is <strong>Francesco&nbsp;Re</strong>.<br>I study <strong>AI/ML Engineering</strong> at <a href='https://www.polimi.it' target='_blank'>PoliMi</a> and in my free time I like to develop apps, bots and AIs.<br><br>$ ";
 
             window.localStorage.lastLogin = new Date();
         })
